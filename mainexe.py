6b0a21e6e881f6138453c4e0662f895b6f6fb2fb
Igor <igor-gesha@yandex.ru>
2023-06-09 19:19:12 +0300

    build for exe

--- mainexe.py
+++ mainexe.py
@@ -2,10 +2,11 @@ import sys
 
 from PySide6.QtWidgets import QApplication
 
-from desktop_version.pyside6.main import MainWindow
+from desktop_version.pyside6.main import MainWindow, on_start_up
 
 
 if __name__ == '__main__':
+    on_start_up()
     app = QApplication(sys.argv)
     form = MainWindow()
     form.show()
